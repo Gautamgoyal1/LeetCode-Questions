d582126cc6ec3b570354b109077f382ea31ec1e7
Gautamgoyal1 <gautamgoyal556@gmail.com>
2026-08-31 23:12:09 +0530

    Time: 4 ms (99.87%), Space: 106.3 MB (42.07%) - LeetHub

---
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[] nodesBetweenCriticalPoints(ListNode head) {
+        int[] arr = {-1,-1};
+        if(head.next.next == null) return arr;
+        int start = 0;
+        int prev = 0;
+        int value = head.val;
+        int maxi = 0;
+        int mini = Integer.MAX_VALUE;
+        head = head.next;
+        int count = 2;
+        while(head.next != null){
+            int curr = head.val;
+            int future = head.next.val;
+            if(curr > value && curr > future){
+                if(prev == 0){
+                    prev = count;
+                    start = count;
+                    count++;
+                    head = head.next;
+                    value = curr;
+                    continue;
+                }
+                else {
+                    maxi = Math.max(maxi,count - start);
+                    mini = Math.min(mini,count-prev);
+                    prev =  count;
+                }
+            }
+            else if(curr < value && curr < future){
+                if(prev == 0){
+                    prev = count;
+                    start = count;
+                    count++;
+                    head = head.next;
+                    value = curr;
+                    continue;
+                }
+                maxi = Math.max(maxi,count - start);
+                mini = Math.min(mini,count-prev);
+                prev =  count;
+            }
+            count++;
+            value = curr;
+            head = head.next;
+        }
+        if(mini == Integer.MAX_VALUE && maxi == 0){
+            return new int[] {-1,-1};
+        }
+        return new int[] {mini,maxi};
+    }
+}
