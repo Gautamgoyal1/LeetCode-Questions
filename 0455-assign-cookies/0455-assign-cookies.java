8aa8849ca050179e41f04fa2a042d5df0ab36708
Gautamgoyal1 <gautamgoyal556@gmail.com>
2026-09-10 22:47:16 +0530

    Time: 14 ms (92.55%), Space: 51.7 MB (6.51%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int n = g.length;
+        int m = s.length;
+        int cnt = 0;
+        int i = 0;
+        int j = 0;
+        while(i<n && j<m){
+            if(g[i] <= s[j]){
+                cnt++;
+                i++;
+                j++;
+            }
+            else j++;
+        }
+        return cnt;
+    }
+}
